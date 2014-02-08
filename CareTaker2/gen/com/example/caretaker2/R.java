d18aabf1d363c37bb0784f81b51d1a013b18faa7
Naveen <naveenkothamasu@gmail.com>
2014-02-08 14:47:37 -0800

    add emergency

--- CareTaker2/gen/com/example/caretaker2/R.java
+++ CareTaker2/gen/com/example/caretaker2/R.java
@@ -24,8 +24,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int edit_message=0x7f080005;
+        public static final int action_settings=0x7f080005;
         public static final int heightVal=0x7f080001;
         public static final int tableRow1=0x7f080000;
         public static final int tableRow2=0x7f080002;
