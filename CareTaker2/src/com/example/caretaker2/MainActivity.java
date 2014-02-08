348d2e1f63a0fe2c45fd3c0d8c24ce50bd763b76
Naveen <naveenkothamasu@gmail.com>
2014-02-08 12:28:32 -0800

    button

--- CareTaker2/src/com/example/caretaker2/MainActivity.java
+++ CareTaker2/src/com/example/caretaker2/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.caretaker2;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+	public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +16,17 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	public void sendMessage(View view) {
+		
+		Intent intent = new Intent(this, ConfigurationActivity.class);
+		EditText edittext = (EditText) findViewById(R.id.edit_message);
+		String message = edittext.getText().toString();
 
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+		
+		/*Intent callIntent = new Intent(Intent.ACTION_CALL);
+		callIntent.setData(Uri.parse("tel:9111"));
+		startActivity(callIntent);*/
+	}
 }
