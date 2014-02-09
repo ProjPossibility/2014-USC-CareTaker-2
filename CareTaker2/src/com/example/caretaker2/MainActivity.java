72c40c72a60ee6ae0b90c43c587465b53dd4b3a8
Naveen <naveenkothamasu@gmail.com>
2014-02-09 10:53:45 -0800

    location part

--- CareTaker2/src/com/example/caretaker2/MainActivity.java
+++ CareTaker2/src/com/example/caretaker2/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
@@ -14,18 +16,30 @@ import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+
+public class MainActivity extends Activity implements
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener {
 
 	public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 	private TextToSpeech ttobj;
+	Location mCurrentLocation;
+	private LocationClient mLocationClient;
+	private static final int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		mLocationClient = new LocationClient(this, this, this);
 		Button button = (Button) findViewById(R.id.emergencyCall);
 		button.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -33,6 +47,15 @@ public class MainActivity extends Activity {
 				callEmergency(view);
 			}
 		});
+		
+		button = (Button) findViewById(R.id.location);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				getLocation(view);
+			}
+		});
+		
 		ttobj = new TextToSpeech(getApplicationContext(),
 				new TextToSpeech.OnInitListener() {
 					@Override
@@ -51,6 +74,18 @@ public class MainActivity extends Activity {
 			ttobj.shutdown();
 		}
 		super.onPause();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mLocationClient.connect();
+	}
+
+	@Override
+	protected void onStop() {
+		mLocationClient.disconnect();
+		super.onStop();
 	}
 
 	public void callEmergency(View view) {
@@ -124,6 +159,7 @@ public class MainActivity extends Activity {
 			intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
 					"Voice recognition Demo...");
 			startActivityForResult(intent, 1234);
+
 		} catch (ActivityNotFoundException e) {
 			Intent browserIntent = new Intent(
 					Intent.ACTION_VIEW,
@@ -131,5 +167,67 @@ public class MainActivity extends Activity {
 			startActivity(browserIntent);
 
 		}
+	}
+
+	private void getLocation(View view) {
+		int gpStatus = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(getApplicationContext());
+		if (gpStatus == ConnectionResult.SUCCESS) {
+		} else {
+			GooglePlayServicesUtil.getErrorDialog(gpStatus, this, 1234);
+		}
+		mCurrentLocation = mLocationClient.getLastLocation();
+		 
+		Toast.makeText(getApplicationContext(), mCurrentLocation.toString(),
+		Toast.LENGTH_SHORT).show();
+		
+		/*TextView textView = new TextView(this);
+		textView.setTextSize(40);
+		textView.setText(mCurrentLocation.toString());*/
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		/*
+		 * Google Play services can resolve some errors it detects. If the error
+		 * has a resolution, try sending an Intent to start a Google Play
+		 * services activity that can resolve error.
+		 */
+		if (connectionResult.hasResolution()) {
+			try {
+				// Start an Activity that tries to resolve the error
+				connectionResult.startResolutionForResult(this,
+						CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+				/*
+				 * Thrown if Google Play services canceled the original
+				 * PendingIntent
+				 */
+			} catch (IntentSender.SendIntentException e) {
+				// Log the error
+				e.printStackTrace();
+			}
+		} else {
+			/*
+			 * If no resolution is available, display a dialog to the user with
+			 * the error.
+			 */
+			// connectionResult.getErrorCode();
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		if(!mLocationClient.isConnected()){
+			mLocationClient.connect();
+		}
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		mLocationClient.getLastLocation();
+	}
+
+	@Override
+	public void onDisconnected() {
+		Toast.makeText(this, "Disconnected. Please re-connect.",
+				Toast.LENGTH_SHORT).show();
 	}
 }
